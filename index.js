eea9476a47ffaf69c833b97fbb2e4ca1eb30cab2
deepakkumarsawpro-hash <deepakkumarsaw.pro@gmail.com>
2026-06-19 23:30:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -148,8 +148,8 @@ async function sendMainMenu(to) {
           body: { text: 'Hi! NearMe me apka swagat hai 👋\n\nKya karna chahte ho?' },
           action: {
             buttons: [
-              { type: 'reply', reply: { id: 'seller_btn', title: 'Seller/Service Register' } },
-              { type: 'reply', reply: { id: 'buyer_btn', title: 'Customer Search' } }
+              { type: 'reply', reply: { id: 'seller_btn', title: 'Seller Register' } },
+              { type: 'reply', reply: { id: 'buyer_btn', title: 'Buyer Search' } }
             ]
           }
         }
